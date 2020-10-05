70a4401292882a40043d2af5be306211efb618f5
frswnaa <fildzahruswinaa@gmail.com>
2020-10-05 10:12:18 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="widt=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Git Remote</title>
 </head>
 <body>
-
+    <h1>Hello world</h1>
+    <p>Ini latihan git remote</p>
 </body>
 </html>
